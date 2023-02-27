0d841abd3e998ddd7958f64879ddaeac128a2037
johess123 <104426729+johess123@users.noreply.github.com>
2023-02-28 00:44:09 +0800

    update_app.js_L55_v4

--- app/app.js
+++ app/app.js
@@ -52,7 +52,7 @@ const handleEvents = async (events = []) => (
       ))
         .map((context) => (!context.error ? handleContext(context) : context)),
     ))
-      .filter((context) => context.messages.length > 0 && context.charAt(0).localeCompare("/") == )
+      .filter((context) => context.messages.length > 0 && context.charAt(0).localeCompare("/") == 0)
       .map((context) => replyMessage(context)),
   ))
 );
